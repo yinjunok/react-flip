6b6e91b9eec2b3dc4dbe0baea304e182ba25e467
root <yinjunok@foxmail.com>
2022-11-30 14:21:34 +0800

    代码优化

--- src/hooks/useFlip.ts
+++ src/hooks/useFlip.ts
@@ -2,6 +2,31 @@ import { useRef, useLayoutEffect } from 'react'
 
 const rectCache = new Map<string, DOMRect>()
 
+type MeasureResult = {
+  x: number
+  y: number
+  scaleX: number
+  scaleY: number
+} | undefined
+
+const measure = (preRect: DOMRect, curRect: DOMRect): MeasureResult => {
+  if (
+    preRect.left !== curRect.left ||
+    preRect.top !== curRect.top ||
+    preRect.width !== curRect.width ||
+    preRect.height !== curRect.height
+  ) {
+    return {
+      x: curRect.left - preRect.left,
+      y: curRect.top - preRect.top,
+      scaleX: curRect.width / preRect.width,
+      scaleY: curRect.height / preRect.height,
+    }
+  }
+
+  return undefined
+}
+
 const useFlip = <T extends HTMLElement = HTMLElement>(layoutId?: string) => {
   const domRef = useRef<T | null>(null)
   const preRect = useRef<DOMRect | undefined>()
@@ -10,17 +35,9 @@ const useFlip = <T extends HTMLElement = HTMLElement>(layoutId?: string) => {
     if (domRef.current) {
       const rect = domRef.current.getBoundingClientRect()
       if (preRect.current) {
-        if (
-          rect.left !== preRect.current.left ||
-          rect.top !== preRect.current.top ||
-          rect.width !== preRect.current.width ||
-          rect.height !== preRect.current.height
-        ) {
-          const disLeft = preRect.current.left - rect.left
-          const disTop = preRect.current.top - rect.top
-          const scaleX = preRect.current.width / rect.width
-          const scaleY = preRect.current.height / rect.height
-          domRef.current.style.transform = `translate3d(${disLeft}px, ${disTop}px, 0) scale(${scaleX}, ${scaleY})`
+        const r = measure(preRect.current, rect)
+        if (r) {
+          domRef.current.style.transform = `translate3d(${r.x}px, ${r.y}px, 0) scale(${r.scaleX}, ${r.scaleY})`
           domRef.current.style.transition = 'none'
 
           requestAnimationFrame(() => {
@@ -40,21 +57,19 @@ const useFlip = <T extends HTMLElement = HTMLElement>(layoutId?: string) => {
     if (layoutId && domRef.current) {
       const prev = rectCache.get(layoutId)
       if (prev) {
-        const rect = domRef.current.getBoundingClientRect()
-        const disLeft = prev.left - rect.left
-        const disTop = prev.top - rect.top
-        const scaleX = prev.width / rect.width
-        const scaleY = prev.height / rect.height
-        domRef.current.style.transform = `translate3d(${disLeft}px, ${disTop}px, 0) scale(${scaleX}, ${scaleY})`
-        domRef.current.style.transition = 'none'
-
-        requestAnimationFrame(() => {
-          if (domRef.current) {
-            domRef.current.style.transition = 'transform .5s ease-in-out'
-            domRef.current.style.transform = 'none'
-            domRef.current.style.transformOrigin = '0 0 0'
-          }
-        })
+        const r = measure(prev, domRef.current.getBoundingClientRect())
+        if (r) {
+          domRef.current.style.transform = `translate3d(${r.x}px, ${r.y}px, 0) scale(${r.scaleX}, ${r.scaleY})`
+          domRef.current.style.transition = 'none'
+  
+          requestAnimationFrame(() => {
+            if (domRef.current) {
+              domRef.current.style.transition = 'transform .5s ease-in-out'
+              domRef.current.style.transform = 'none'
+              domRef.current.style.transformOrigin = '0 0 0'
+            }
+          })
+        }
       }
     }
 
